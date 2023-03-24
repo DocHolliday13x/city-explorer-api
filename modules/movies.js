480a12394b48f0771ca314156bbd7bdc104b6acb
Ryan Eastman <rking1323@gmail.com>
2023-03-24 22:05:52 +0000

    met lab10 requirements w/o stretch goals

--- modules/movies.js
+++ modules/movies.js
@@ -2,27 +2,49 @@
 
 const axios = require('axios');
 
+let cache = {};
+
+// TODO: need to create a key for the data I am going to store
+// TODO: if the thing exists AND within a valid timeframe... send data from cache
+// TODO: if the thing does NOT exist... call API and cache that return from the API
+
 async function getMoviesData(request, response, next) {
 
   try {
 
     let keywordFromFrontEnd = request.query.searchQuery;
 
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIEDB_API_KEY}&language=en-US&page=1&include_adult=false&query=${keywordFromFrontEnd}`;
+    let key = `${keywordFromFrontEnd}-Movies`; // key = Seattle-movies
 
-    let movieResults = await axios.get(url);
+    if (cache[key] && (Date.now() - cache[key].timestamp) < 4.32e+7) {
+      console.log('Cache was hit!');
 
-    // *** GROOMED DATA TO SEND TO FRONT-END ***
-    let mappedMoviesDataToSend = movieResults.data.results.map(movie => {
-      return new Movies(movie);
-    });
-    console.log(mappedMoviesDataToSend);
-    response.status(200).send(mappedMoviesDataToSend);
+    } else {
+
+      console.log('Movie data is not in cache!');
+      let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIEDB_API_KEY}&language=en-US&page=1&include_adult=false&query=${keywordFromFrontEnd}`;
+
+      let movieResults = await axios.get(url);
+
+      let mappedMoviesDataToSend = movieResults.data.results.map(movies => {
+        return new Movies(movies);
+      });
+
+      // *** Build It Into Cache ***
+      cache[key] = {
+        data: mappedMoviesDataToSend,
+        timestamp: Date.now()
+      };
+
+      response.status(200).send(mappedMoviesDataToSend);
+
+    }
+
   } catch (error) {
     next(error);
   }
-
 }
+
 
 // *** BUILD CLASS TO TRIM DOWN DATA ***
 class Movies {
@@ -32,6 +54,7 @@ class Movies {
     this.image = `https://image.tmdb.org/t/p/w500${movieObj.poster_path}`;
   }
 }
+
 
 
 
--- modules/weather.js
+++ modules/weather.js
@@ -2,22 +2,47 @@
 
 const axios = require('axios');
 
+let cache = {};
+
+// TODO: need to create a key for the data I am going to store
+// TODO: if the thing exists AND within a valid timeframe... send data from cache
+// TODO: if the thing does NOT exist... call API and cache that return from the API
+
 async function getWeatherData(req, res, next) {
 
   try {
+
     let lat = req.query.lat;
     let lon = req.query.lon;
-    // console.log(request.query);
+    let key = `lat:${lat} lon: ${lon}-Weather`; // key = Seattle-weather
 
-    let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.REACT_APP_WEATHERBIT_API_KEY}&days=7&lat=${lat}&lon=${lon}`;
+    if (cache[key] && (Date.now() - cache[key].timestamp) < 4.32e+7) {
+      console.log('Cache was hit!');
 
-    let weatherData = await axios.get(url);
+      res.status(200).send(cache[key].data);
 
-    let mappedWeatherDataToSend = weatherData.data.data.map(dailyForecast => {
-      return new Forecast(dailyForecast);
-    });
+    } else {
 
-    res.status(200).send(mappedWeatherDataToSend);
+      console.log('Weather data is not in the cache!');
+
+      let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.REACT_APP_WEATHERBIT_API_KEY}&days=7&lat=${lat}&lon=${lon}`;
+
+      let weatherData = await axios.get(url);
+
+      let mappedWeatherDataToSend = weatherData.data.data.map(dailyForecast => {
+        return new Forecast(dailyForecast);
+      });
+
+      // *** Build It Into Cache ***
+      cache[key] = {
+        data: mappedWeatherDataToSend,
+        timestamp: Date.now()
+      };
+
+      res.status(200).send(mappedWeatherDataToSend);
+
+    }
+
   } catch (error) {
     next(error);
   }
